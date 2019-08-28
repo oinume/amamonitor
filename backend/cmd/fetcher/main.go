af653f82842224d2f22ec6e130c415617ff896a4
Kazuhiro Oinuma <oinume@gmail.com>
2019-08-28 20:36:00 +0900

    Refactor main

--- backend/cmd/fetcher/main.go
+++ backend/cmd/fetcher/main.go
@@ -3,15 +3,59 @@
 package main
 
 import (
-	"context"
+	"flag"
 	"fmt"
-	"log"
+	"io"
+	"net/http"
 	"os"
-	"time"
 
-	"github.com/chromedp/chromedp"
+	"github.com/oinume/amamonitor/cli"
 )
 
+func main() {
+	m := &fetcherMain{
+		outStream: os.Stdout,
+		errStream: os.Stderr,
+	}
+	if err := m.run(os.Args); err != nil {
+		cli.WriteError(m.errStream, err)
+		os.Exit(cli.ExitError)
+	}
+	os.Exit(cli.ExitOK)
+}
+
+type fetcherMain struct {
+	outStream io.Writer
+	errStream io.Writer
+}
+
+func (m *fetcherMain) run(args []string) error {
+	flagSet := flag.NewFlagSet("fetcher", flag.ContinueOnError)
+	flagSet.SetOutput(m.errStream)
+	var (
+		server = flagSet.Bool("server", false, "Run as a HTTP server")
+		//concurrency     = flagSet.Int("concurrency", 1, "Concurrency of crawler. (default=1)")
+		//continueOnError = flagSet.Bool("continue", true, "Continue to crawl if any error occurred. (default=true)")
+		//specifiedIDs    = flagSet.String("ids", "", "Teacher IDs")
+		//followedOnly    = flagSet.Bool("followedOnly", false, "Crawl followedOnly teachers")
+		//all             = flagSet.Bool("all", false, "Crawl all teachers ordered by evaluation")
+		//newOnly         = flagSet.Bool("new", false, "Crawl all teachers ordered by new")
+		//interval        = flagSet.Duration("interval", 1*time.Second, "Fetch interval. (default=1s)")
+		//logLevel        = flag.String("log-level", "info", "Log level")
+	)
+	if err := flagSet.Parse(args[1:]); err != nil {
+		return err
+	}
+
+	if *server {
+		port := os.Getenv("PORT")
+		return http.ListenAndServe(fmt.Sprintf(":%s", port), nil)
+	}
+
+	return nil
+}
+
+/*
 func main() {
 	// create chrome instance
 	ctx, cancel := chromedp.NewContext(
@@ -42,3 +86,4 @@ func main() {
 	}
 	log.Printf("Go's time.After example:\n%s", example)
 }
+*/
