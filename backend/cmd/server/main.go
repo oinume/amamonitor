77c4bc3962e013d45f959029f51ac2aab0c30eaa
Kazuhiro Oinuma <oinume@gmail.com>
2019-10-02 22:54:20 +0900

    Use model and service

--- backend/cmd/server/main.go
+++ backend/cmd/server/main.go
@@ -9,6 +9,8 @@ import (
 
 	"github.com/oinume/amamonitor/backend/cli"
 	"github.com/oinume/amamonitor/backend/http_server"
+	"github.com/oinume/amamonitor/backend/service"
+	"github.com/xo/dburl"
 )
 
 func main() {
@@ -39,11 +41,18 @@ func (m *serverMain) run(args []string) error {
 		return err
 	}
 
+	// TODO: config
+	db, err := dburl.Open("mysql://user:pass@host/?parseTime=true&sql_mode=ansi")
+	if err != nil {
+		return err
+	}
+	svc := service.New(db)
+	server := http_server.New(db, svc)
+
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "5001"
 	}
-	server := http_server.New()
 	fmt.Printf("Listening on port %v\n", port)
 	return http.ListenAndServe(fmt.Sprintf(":%s", port), server.NewRouter())
 }
