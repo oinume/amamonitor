d46999e5a959ac65ff470a2f82ed5d2b99f5c4f2
Kazuhiro Oinuma <oinume@gmail.com>
2019-10-28 16:26:54 +0900

    Implement giftissue client

--- backend/fetcher/giftissue.go
+++ backend/fetcher/giftissue.go
@@ -2,8 +2,11 @@ package fetcher
 
 import (
 	"context"
+	"fmt"
 	"io"
 	"net/http"
+	"strconv"
+	"strings"
 
 	"gopkg.in/xmlpath.v2"
 )
@@ -37,11 +40,69 @@ func (c *giftissueClient) getHeaders() map[string]string {
 }
 
 func (c *giftissueClient) parse(body io.Reader) ([]*GiftItem, error) {
-	_, err := xmlpath.ParseHTML(body)
+	giftListItemXPath := xmlpath.MustCompile(`//table/tbody/tr[@class='giftList_item']`)
+	faceValueXPath := xmlpath.MustCompile(`td[contains(@class, 'giftList_cell-facevalue')]/span`)
+	priceXPath := xmlpath.MustCompile(`td[contains(@class, 'giftList_cell-price')]/span`)
+	rateXPath := xmlpath.MustCompile(`td[contains(@class, 'giftList_rate')]/span`)
+	root, err := xmlpath.ParseHTML(body)
 	if err != nil {
 		return nil, err
 	}
-	return nil, nil
+
+	giftItems := make([]*GiftItem, 0, 100)
+	for iter := giftListItemXPath.Iter(root); iter.Next(); {
+		node := iter.Node()
+		faceValueString, ok := faceValueXPath.String(node)
+		if !ok {
+			continue
+		}
+		faceValue, err := c.normalizeAmount(faceValueString)
+		if err != nil {
+			return nil, fmt.Errorf("failed to parse faceValue: %v", err)
+		}
+
+		priceString, ok := priceXPath.String(node)
+		if !ok {
+			continue
+		}
+		price, err := c.normalizeAmount(priceString)
+		if err != nil {
+			return nil, fmt.Errorf("failed to parse price: %v", err)
+		}
+
+		rateString, ok := rateXPath.String(node)
+		if !ok {
+			continue
+		}
+		rate, err := c.normalizeRate(rateString)
+		if err != nil {
+			return nil, fmt.Errorf("failed to parse rate: %v", err)
+		}
+
+		giftItem := NewGiftItem(GiftissueProvider, rate, faceValue, price)
+		//fmt.Printf("giftItem = %+v\n", giftItem)
+		giftItems = append(giftItems, giftItem)
+	}
+
+	return giftItems, nil
+}
+
+func (c *giftissueClient) normalizeAmount(s string) (uint, error) {
+	s = strings.ReplaceAll(s, "¥", "")
+	s = strings.ReplaceAll(s, ",", "")
+	s = strings.TrimSpace(s)
+	v, err := strconv.ParseUint(s, 10, 64)
+	if err != nil {
+		return 0, err
+	}
+	return uint(v), nil
+}
+
+func (c *giftissueClient) normalizeRate(s string) (string, error) {
+	v := strings.ReplaceAll(s, "%", "")
+	v = strings.TrimSpace(v)
+	// TODO: validate
+	return v, nil
 }
 
 func (c *giftissueClient) Fetch(ctx context.Context, options *FetchOptions) ([]*GiftItem, error) {
---
+++ backend/fetcher/giftissue_test.go
@@ -0,0 +1,57 @@
+package fetcher
+
+import (
+	"context"
+	"fmt"
+	"testing"
+)
+
+func Test_Fetcher_GiftissueClient(t *testing.T) {
+	//ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	//	w.Header().Set("Content-Type", "application/json")
+	//	w.WriteHeader(http.StatusOK)
+	//
+	//	gifts := []AmatenGift{
+	//		{
+	//			ID:        123,
+	//			FaceValue: 10000,
+	//			Price:     8710,
+	//			Rate:      "87.1",
+	//		},
+	//		{
+	//			ID:        456,
+	//			FaceValue: 1000,
+	//			Price:     900,
+	//			Rate:      "90.0",
+	//		},
+	//	}
+	//	response := AmatenGiftResponse{
+	//		Gifts: gifts,
+	//	}
+	//
+	//	if err := json.NewEncoder(w).Encode(&response); err != nil {
+	//		t.Fatalf("Encode() failed: %v", err)
+	//	}
+	//}))
+	//defer ts.Close()
+
+	f := NewWithClient(NewGiftissueClient())
+	giftItems, err := f.Fetch(context.Background(), &FetchOptions{
+		//URL: ts.URL,
+	})
+	if err != nil {
+		t.Fatalf("Fetch failed: %v", err)
+	}
+	fmt.Printf("giftItems = %+v\n", giftItems)
+
+	//want := []*GiftItem{
+	//	NewGiftItem(AmatenProvider, "87.1", 10000, 8710),
+	//	NewGiftItem(AmatenProvider, "90.0", 1000, 900),
+	//}
+	//if len(giftItems) != len(want) {
+	//	t.Fatalf("unexpected Fetch result giftItems length")
+	//}
+	//if got := giftItems; !reflect.DeepEqual(got, want) {
+	//	t.Errorf("unexpected Fetch result giftItems: got=%+v, want=%+v", got, want)
+	//}
+}
