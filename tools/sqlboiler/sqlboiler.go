d4b4c49b0bced2d52a3af837396ec01ac6a09b46
Kazuhiro Oinuma <oinume@gmail.com>
2020-03-14 22:53:55 +0900

    Add sqlboiler config generator

---
+++ tools/sqlboiler/sqlboiler.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	t := `
+[mysql]
+  dbname  = "%s"
+  host    = "%s"
+  port    = %s
+  user    = "%s"
+  pass    = "%s"
+  sslmode = "false"
+`
+	s := fmt.Sprintf(
+		t,
+		getenvDefault("MYSQL_DATABASE", "hoge"),
+		getenvDefault("MYSQL_HOST", "localhost"),
+		getenvDefault("MYSQL_PORT", "3306"),
+		getenvDefault("MYSQL_USER", "root"),
+		getenvDefault("MYSQL_PASSWORD", "root"),
+	)
+	s = strings.TrimSpace(s)
+	fmt.Println(s)
+}
+
+func getenvDefault(name, d string) string {
+	v := os.Getenv(name)
+	if v == "" {
+		return d
+	}
+	return v
+}
